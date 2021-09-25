e3cb44dfc485e3ed90ac923241e03e90dd2f2eda
Nehal <sahilnarvekar8@gmail.com>
2021-09-25 15:55:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send({ hi: ' there' });
 });
-*/
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
