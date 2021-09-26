7a9d9fa67c5ab0fb82c63eba393b7429b64f91ae
Nehal9922940053 <80582112+Nehal9922940053@users.noreply.github.com>
2021-09-26 23:23:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: ' there' });
+app.listen(5000, () => {
+    console.log("Application started Successfully");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
-
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
