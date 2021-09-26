aa0153113a0f619535935e30bca20f0680fa6653
Nehal <sahilnarvekar8@gmail.com>
2021-09-26 22:39:16 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,4 +8,17 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+*/
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    })
+    fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 5000)
 
